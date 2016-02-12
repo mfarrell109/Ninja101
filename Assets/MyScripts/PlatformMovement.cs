16cc21a86b74ed01e7244e625aa9ddfe105975b5
BrianErikson <briankerikson@gmail.com>
2016-02-12 20:04:46 +0100

    #1 #2 created prefab platform that lerps between an origin and an offset on both sides. Player does not stick to platform yet

---
+++ Assets/MyScripts/PlatformMovement.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformMovement : MonoBehaviour {
+    public bool vertical = false;
+    public float offset = 1.0f;
+    public float speed = 1.0f;
+    
+    private Vector3 start = new Vector3();
+    private Vector3 end = new Vector3();
+    private float moveDistance;
+
+
+	// Use this for initialization
+	void Start () {
+        setupTrack();
+        moveDistance = Vector3.Distance(start, end);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float perCovered = Mathf.Abs((Mathf.Sin(Time.time) * 0.5f) + 0.5f);
+
+        // apply
+        transform.position = Vector3.Lerp(start, end, perCovered);
+	}
+
+    void OnDrawGizmosSelected () {
+        Gizmos.color = Color.gray;
+
+        setupTrack();
+
+        Gizmos.DrawLine(start, end);
+    }
+
+    // Setup starting and ending positions of the track to slerp between
+    void setupTrack () {
+        Vector3 pos = transform.position;
+        start.Set(pos.x, pos.y, pos.z);
+        end.Set(pos.x, pos.y, pos.z);
+
+        if (vertical) {
+            start.y -= offset;
+            end.y += offset;
+        }
+        else {
+            start.x -= offset;
+            end.x += offset;
+        }
+    }
+}
