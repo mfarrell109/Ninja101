b0a2506098d2b1fc4978ef1101adf68afa0982e9
BrianErikson <briankerikson@gmail.com>
2016-02-15 10:04:18 +0100

    centered ninja4 sprite, created throwable shruikens and associated AI action, added more comments, added death animation

---
+++ Assets/Code/ShruikenBehavior.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShruikenBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void OnTriggerEnter2D (Collider2D other) // Doesn't matter what it hits -- gets destroyed anyway
+    {
+        Destroy(gameObject);
+    }
+}
