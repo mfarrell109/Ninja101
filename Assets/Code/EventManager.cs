9a4442fce9d8941de16fc307963007ae43f6fc8e
BrianErikson <briankerikson@gmail.com>
2016-02-28 11:37:09 +0100

    init EventManager with unhooked PlayerHit event sample

---
+++ Assets/Code/EventManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventManager : MonoBehaviour {
+
+    public delegate void OnPlayerHit();
+
+    public OnPlayerHit onPlayerHit;
+
+
+    public void PlayerHit(int dmg)
+    {
+        if (onPlayerHit != null)
+        {
+            onPlayerHit();
+        }
+    }
+}
