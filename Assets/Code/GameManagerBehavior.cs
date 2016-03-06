6a9069e9aab8dbad39c8381f7dc8d506d8ad8872
BrianErikson <briankerikson@gmail.com>
2016-03-06 15:07:30 +0100

    FB login is now working

--- Assets/Code/GameManagerBehavior.cs
+++ Assets/Code/GameManagerBehavior.cs
@@ -2,27 +2,80 @@
 using System.Collections;
 using System.Collections.Generic;
 using Facebook.Unity;
+using System;
+
+namespace UnityEngine
+{
+    public enum UserLoginType
+    {
+        Facebook,
+        GooglePlus
+    }
+
+    public interface NinjaUser
+    {
+        string GetFirstName();
+        string GetLastName();
+        UserLoginType GetLoginType();
+        // TODO Texture GetProfilePicture();
+    }
+
+    public struct FbNinjaUser : NinjaUser
+    {
+        private string firstName;
+        private string lastName;
+        private AccessToken accessToken;
+
+        public FbNinjaUser(string first, string last, AccessToken token)
+        {
+            firstName = first;
+            lastName = last;
+            accessToken = token;
+        }
+
+        string NinjaUser.GetFirstName()
+        {
+           return firstName;
+        }
+
+        string NinjaUser.GetLastName()
+        {
+            return lastName;
+        }
+
+        UserLoginType NinjaUser.GetLoginType()
+        {
+            return UserLoginType.Facebook;
+        }
+
+        public AccessToken GetFbAccessToken()
+        {
+            return accessToken;
+        }
+    }
+}
 
 public class GameManagerBehavior : MonoBehaviour
 {
+    public NinjaUser user;
+
 
     void Awake()
     {
         DontDestroyOnLoad(this);
     }
-
-	// Use this for initialization
+    
 	void Start()
     {
 	
 	}
 	
-	// Update is called once per frame
 	void Update()
     {
 	
 	}
 
+    // Call this method to initiate FB login and to query user for permission
     public void FacebookLogin()
     {
         if (!FB.IsInitialized)
@@ -31,13 +84,14 @@ public class GameManagerBehavior : MonoBehaviour
         }
     }
 
+    // This is called after the FB SDK has been initialized
     private void FbInitCallback()
     {
         if (FB.IsInitialized)
         {
             Debug.Log("FB SDK Initialized");
             FB.ActivateApp();
-            var permissions = new List<string>() {"public_profile"};
+            var permissions = new List<string>() {"public_profile", "user_friends"};
             Debug.Log("Logging in with FB...");
             FB.LogInWithReadPermissions(permissions, FbAuthCallback);
         }
@@ -47,12 +101,12 @@ public class GameManagerBehavior : MonoBehaviour
         }
     }
 
+    // Called after the user has authenticated
     private void FbAuthCallback(ILoginResult result)
     {
         if (FB.IsLoggedIn)
         {
-            AccessToken token = Facebook.Unity.AccessToken.CurrentAccessToken;
-            Debug.Log("Welcome, " + token.UserId);
+            FB.API("/me?fields=first_name,last_name", Facebook.Unity.HttpMethod.GET, FbProfileCallback);
         }
         else
         {
@@ -60,6 +114,37 @@ public class GameManagerBehavior : MonoBehaviour
         }
     }
 
+    // Acquires and builds a newly logged-in user
+    private void FbProfileCallback(IGraphResult result)
+    {
+        if (result.Error != null)
+        {
+            Debug.LogError("FB Error: " + result.Error);
+        }
+        else if (!FB.IsLoggedIn)
+        {
+            Debug.LogError("FB Login cancelled by player");
+        }
+        else
+        {
+            string firstName = result.ResultDictionary["first_name"].ToString();
+            string lastName = result.ResultDictionary["last_name"].ToString();
+            user = new FbNinjaUser(firstName, lastName, Facebook.Unity.AccessToken.CurrentAccessToken);
+            GreetUser();
+
+            Application.LoadLevel("GameMenu");
+        }
+    }
+
+    private void GreetUser()
+    {
+        if (user != null)
+        {
+            Debug.Log("Welcome, " + user.GetFirstName() + " " + user.GetLastName() + ". You are now authenticated with " + user.GetLoginType().ToString() + ".");
+        }
+    }
+
+    // TODO: Pulled from FB SDK example. Do we actually need this?
     private void OnHideUnity (bool isGameShown)
     {
         if (!isGameShown)
