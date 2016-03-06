06c84ad82f1a534000407fd111c35af479bc85e5
BrianErikson <briankerikson@gmail.com>
2016-03-06 21:23:36 +0100

    Allowing possibility of cached logins on mobile platforms for FB, and cached login working for GS

--- Assets/Code/GameManagerBehavior.cs
+++ Assets/Code/GameManagerBehavior.cs
@@ -3,7 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using Facebook.Unity;
 using GameSparks.Api.Requests;
-using GameSparks.Api.Responses;
+using GameSparks.Core;
+using GameSparks.Platforms;
 using System;
 
 namespace UnityEngine
@@ -59,23 +60,74 @@ namespace UnityEngine
 
 public class GameManagerBehavior : MonoBehaviour
 {
+    public static class UserPrefKeys
+    {
+        public const string USER_ID = "UserId";
+        public const string FIRST_NAME = "FirstName";
+        public const string LAST_NAME = "LastName";
+        public const string LOGIN_TYPE = "LoginType";
+        public const string CACHED_ID = "CachedId";
+    }
+
+    private class CachedUser
+    {
+        public string firstName;
+        public string lastName;
+        public UserLoginType loginType;
+        public string cachedId;
+    }
+    
     public NinjaUser user;
 
 
     void Awake()
     {
         DontDestroyOnLoad(this);
+
+        // Initialize FB to see if there's a valid session
+        if (!FB.IsInitialized)
+        {
+            FB.Init(() =>
+            {
+                if (FB.IsInitialized && FB.IsLoggedIn) // There's a valid session!
+                {
+                    if (Application.isMobilePlatform)
+                    {
+                        FB.ActivateApp();
+                    }
+                    Debug.Log("Found valid FB Session. Re-using.");
+                    CachedUser cUser = GetCachedUser();
+                    if (cUser.loginType == UserLoginType.Facebook && cUser.cachedId == Facebook.Unity.AccessToken.CurrentAccessToken.UserId)
+                    {
+                        SetUser(cUser.firstName, cUser.lastName, UserLoginType.Facebook, Facebook.Unity.AccessToken.CurrentAccessToken.UserId);
+                        OnSocialMediaLogin();
+                    }
+                    else // attempt to get correct details for FB session
+                    {
+                        FbInitCallback();
+                    }
+                }
+            },
+            OnHideUnity);
+        }
     }
     
 	void Start()
     {
-	
-	}
+
+    }
 	
 	void Update()
     {
 	
 	}
+
+    private void OnSocialMediaLogin()
+    {
+        GreetUser();
+        GameSparksLogin();
+        Application.LoadLevel("GameMenu");
+    }
 
     // Call this method to initiate FB login and to query user for permission
     public void FacebookLogin()
@@ -84,6 +136,10 @@ public class GameManagerBehavior : MonoBehaviour
         {
             FB.Init(FbInitCallback, OnHideUnity);
         }
+        else
+        {
+            FbInitCallback();
+        }
     }
 
     // This is called after the FB SDK has been initialized
@@ -91,10 +147,12 @@ public class GameManagerBehavior : MonoBehaviour
     {
         if (FB.IsInitialized)
         {
-            Debug.Log("FB SDK Initialized");
-            FB.ActivateApp();
+            if (Application.isMobilePlatform)
+            {
+                FB.ActivateApp();
+            }
             var permissions = new List<string>() {"public_profile", "user_friends"};
-            Debug.Log("Logging in with FB...");
+            Debug.Log("Logging in with Facebook...");
             FB.LogInWithReadPermissions(permissions, FbAuthCallback);
         }
         else
@@ -108,35 +166,29 @@ public class GameManagerBehavior : MonoBehaviour
     {
         if (FB.IsLoggedIn)
         {
-            FB.API("/me?fields=first_name,last_name", Facebook.Unity.HttpMethod.GET, FbProfileCallback);
+            FB.API("/me?fields=first_name,last_name", Facebook.Unity.HttpMethod.GET, (getResult) =>
+            {
+                if (getResult.Error != null)
+                {
+                    Debug.LogError("FB Error: " + getResult.Error);
+                }
+                else if (!FB.IsLoggedIn)
+                {
+                    Debug.LogError("FB Login cancelled by player");
+                }
+                // Generate new user, log into GameSparks, and load the Main Menu
+                else
+                {
+                    string firstName = getResult.ResultDictionary["first_name"].ToString();
+                    string lastName = getResult.ResultDictionary["last_name"].ToString();
+                    user = new FbNinjaUser(firstName, lastName, Facebook.Unity.AccessToken.CurrentAccessToken);
+                    OnSocialMediaLogin();
+                }
+            });
         }
         else
-        {
-            Debug.LogError("User either cancelled FB login, the app could not connect to the FB servers, or something bad happened...");
-        }
-    }
-
-    // Acquires and builds a newly logged-in user
-    private void FbProfileCallback(IGraphResult result)
-    {
-        if (result.Error != null)
         {
             Debug.LogError("FB Error: " + result.Error);
-        }
-        else if (!FB.IsLoggedIn)
-        {
-            Debug.LogError("FB Login cancelled by player");
-        }
-        else
-        {
-            string firstName = result.ResultDictionary["first_name"].ToString();
-            string lastName = result.ResultDictionary["last_name"].ToString();
-            user = new FbNinjaUser(firstName, lastName, Facebook.Unity.AccessToken.CurrentAccessToken);
-            GreetUser();
-
-            GameSparksLogin();
-
-            Application.LoadLevel("GameMenu");
         }
     }
 
@@ -150,32 +202,79 @@ public class GameManagerBehavior : MonoBehaviour
 
     private void GameSparksLogin()
     {
-        if (FB.IsLoggedIn)
+        if (!GS.Available) // Attempt to return GameSparks to an available state. For some reason GS hangs on connecting occassionally.
+        {
+            Debug.Log("The GameSparks service is unavailable. Attempting to reset.");
+            GS.Reset();
+        }
+
+        if (FB.IsLoggedIn && !GS.Authenticated)
         {
             new FacebookConnectRequest().SetAccessToken(Facebook.Unity.AccessToken.CurrentAccessToken.TokenString).Send((response) =>
             {
                 if (response.HasErrors)
                 {
-                    Debug.Log("Something failed when connecting with Facebook: " + response.Errors.JSON);
+                    Debug.Log("Something failed when connecting to GameSparks with Facebook: " + response.Errors.JSON);
                 }
                 else
                 {
                     //Otherwise we are successfully logged in!
-                    Debug.Log("Gamesparks Facebook Login Successful");
+                    Debug.Log("Gamesparks Facebook Login Successful. " + response.DisplayName);
                     //Since we successfully logged in, we can get our account information.
 
                 }
             });
-        } 
+        }
+        else if (FB.IsLoggedIn && GS.Authenticated)
+        {
+            Debug.Log("Detected cached GameSparks session ID " + GS.GSPlatform.UserId + ". No need for re-authentication.");
+        }
         // Else if G+ login
     }
 
-    private void response(AuthenticationResponse obj)
+    // Retrieves the last logged on user from the cache
+    // Returns null if there was a problem
+    private CachedUser GetCachedUser()
     {
-        throw new NotImplementedException();
+        CachedUser cachedUser = new CachedUser();
+        try
+        {
+            cachedUser.firstName = PlayerPrefs.GetString(UserPrefKeys.FIRST_NAME);
+            cachedUser.lastName = PlayerPrefs.GetString(UserPrefKeys.LAST_NAME);
+            cachedUser.loginType = (UserLoginType)Enum.Parse(typeof(UserLoginType), PlayerPrefs.GetString(UserPrefKeys.LOGIN_TYPE));
+            cachedUser.cachedId = PlayerPrefs.GetString(UserPrefKeys.CACHED_ID);
+        }
+        catch (Exception e)
+        {
+            cachedUser = null;
+        }
+
+        return cachedUser;
     }
 
-    // TODO: Pulled from FB SDK example. Do we actually need this?
+    private void SetUser(string firstName, string lastName, UserLoginType loginType, string userId)
+    {
+        PlayerPrefs.SetString(UserPrefKeys.FIRST_NAME, firstName);
+        PlayerPrefs.SetString(UserPrefKeys.LAST_NAME, lastName);
+        PlayerPrefs.SetString(UserPrefKeys.LOGIN_TYPE, loginType.ToString());
+        PlayerPrefs.SetString(UserPrefKeys.CACHED_ID, userId);
+        PlayerPrefs.Save();
+
+        if (loginType == UserLoginType.Facebook)
+        {
+            if (FB.IsLoggedIn)
+            {
+                user = new FbNinjaUser(firstName, lastName, Facebook.Unity.AccessToken.CurrentAccessToken);
+            }
+            else
+            {
+                Debug.Log("Warning: Caching a user to PlayerPrefs that isn't actually logged in at this moment");
+            }
+        }
+        // else if G+
+    }
+
+    // Pauses the game in the background while FB is being authorized
     private void OnHideUnity (bool isGameShown)
     {
         if (!isGameShown)
